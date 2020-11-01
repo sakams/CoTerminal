d85f6ab335d0582dae64733864479a279e282ede
sakams <mssaka29@gmail.com>
2020-11-01 19:09:08 +0000

    user model

---
+++ coterminal/src/main/java/com/model/User.java
@@ -0,0 +1,103 @@
+package com.bitfall.coterminal.model;
+
+import com.bitfall.coterminal.model.audit.DateAudit;
+import org.hibernate.annotations.NaturalId;
+import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "users", uniqueConstraints = {
+        @UniqueConstraint(columnNames = {
+            "username"
+        }),
+        @UniqueConstraint(columnNames = {
+            "email"
+        })
+})
+public class User extends DateAudit {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    @Size(max = 40)
+    private String name;
+
+    @NotBlank
+    @Size(max = 40)
+    private String firstname;
+
+    @NotBlank
+    @Size(max = 40)
+    private String lastname;
+
+    @NaturalId
+    @NotBlank
+    @Size(max = 40)
+    @Email
+    private String email;
+
+    @NotBlank
+    @Size(max = 100)
+    private String password;
+
+    
+
+    public User() {
+
+    }
+
+    public User(String firstname, String lastname,String email, String password) {
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.email = email;
+        this.password = password;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    
+
+    public String getfirstName() {
+        return firstname;
+    }
+
+    public void setfirstName(String firstname) {
+        this.firstname = firstname;
+    }
+    public String getlastName() {
+        return lastname;
+    }
+
+    public void setlastName(String lastname) {
+        this.name = lastname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+  
+}
